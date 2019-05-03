f5b445ec4f6be3ff6c53200179044734a132b758
Mateo Tutic <mateo@tutic.ch>
2019-05-03 15:27:44 +0200

    Die class implemented

---
+++ src/test/java/ch/heigvd/gen/monopoly/DieTest.java
@@ -0,0 +1,23 @@
+package ch.heigvd.gen.monopoly;
+
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class DieTest {
+    @Test
+    void faceValueShouldBeZeroWithoutRoll() {
+        Die d = new Die();
+        assertEquals(0, d.getFaceValue());
+    }
+
+    @RepeatedTest(50)
+    void faceValueShouldBeBetween1andUpperBound() {
+        Die d = new Die();
+
+        d.roll();
+        assertTrue(d.getFaceValue() >= 1 && d.getFaceValue() <= d.getUpperBound());
+    }
+}
