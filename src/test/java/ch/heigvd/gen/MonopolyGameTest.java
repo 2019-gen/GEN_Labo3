1cd44583719e27466f2e3062e02fed8baf5257f6
Loris Gilliand <loris.gilliand@heig-vd.ch>
2019-05-04 14:08:03 +0200

    MonopolyGame with test

--- src/test/java/ch/heigvd/gen/MonopolyGameTest.java
+++ src/test/java/ch/heigvd/gen/MonopolyGameTest.java
@@ -1,17 +1,27 @@
 package ch.heigvd.gen;
 
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
- * Unit test for simple App.
+ * Unit test for simple MononpolyGame.
  */
-public class AppTest {
-    /**
-     * Rigorous Test :-)
-     */
+public class MonopolyGameTest {
     @Test
-    public void shouldAnswerWithTrue() {
-        assertTrue( true );
+    public void numberOfPlayerShouldBeAcceptable() {
+        String[] OnePlayer = new String[1];
+        String[] TenPlayer = new String[10];
+
+        // trop peu de joueurs
+        assertThrows(IllegalArgumentException.class, () -> {
+            new MonopolyGame();
+        });
+
+        // trop de joeurs
+        assertThrows(IllegalArgumentException.class, () -> {
+            new MonopolyGame(TenPlayer);
+        });
     }
 }
