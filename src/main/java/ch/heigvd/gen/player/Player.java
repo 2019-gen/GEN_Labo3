7c5aa886389675ba58f414c3af029498ac4d2810
Laykel <layke.luc@gmail.com>
2019-05-05 19:54:08 +0200

    Added javadoc in Player class

--- src/main/java/ch/heigvd/gen/player/Player.java
+++ src/main/java/ch/heigvd/gen/player/Player.java
@@ -5,25 +5,47 @@ import ch.heigvd.gen.monopoly.dice.Cup;
 import ch.heigvd.gen.monopoly.Piece;
 import ch.heigvd.gen.monopoly.square.Square;
 
+/**
+ * Cette classe représente un joueur de monopoly
+ *
+ * @author Loris Gilliand
+ * @version 1.0
+ * @since 05.05.2019
+ */
 public class Player {
-    private final String name;
-    private final Piece piece;
-    private int cash;
+    private final String name; // Nom du joueur
+    private final Piece piece; // Pièce déplacée par le joueur
+    private int cash; // Argent en possession du joueur
 
+    /**
+     * Constructeur de joueur
+     *
+     * @param name Le nom du joueur
+     * @param pieceName La pièce déplacée par le joueur
+     * @param startSquare La case sur laquelle le joueur va commencer
+     */
     public Player(String name, String pieceName, Square startSquare) {
         this.name = name;
         this.piece = new Piece(pieceName, startSquare);
         this.cash = 1500;
     }
 
+    /**
+     * Méthode gérant le tour du joueur
+     *
+     * @param board Le plateau de jeu
+     * @param cup Le bol pour lancer les dés
+     */
     public void takeTurn(Board board, Cup cup) {
         System.out.println(this + " commence son tour...");
 
+        // Le joueur lance les dés
         cup.roll();
         int score = cup.getTotal();
 
         System.out.println(this + " a fait un lancé de " + score);
 
+        // La pièce est déplacée
         Square oldLocation = piece.getLocation();
         piece.setLocation(board.getSquare(oldLocation, score));
 
@@ -31,28 +53,56 @@ public class Player {
                 + piece.getLocation());
     }
 
+    /**
+     * Surcharge de toString pour l'affichage d'un joueur
+     *
+     * @return Le nom du joueur
+     */
+    @Override
     public String toString() {
         return name;
     }
 
-    public void addCash(int value) {
+    /**
+     * Ajouter de l'argent au joueur
+     *
+     * @param value Le montant à ajouter au joueur
+     * @throws IllegalArgumentException En cas de montant négatif
+     */
+    public void addCash(int value) throws IllegalArgumentException {
         if (cash < 0) {
-            throw new IllegalArgumentException("Impossible d'ajouter du cash negatif");
+            throw new IllegalArgumentException("Impossible d'ajouter du cash négatif");
         }
         cash += value;
     }
 
+    /**
+     * Retirer de l'argent au joueur
+     *
+     * @param value Le montant à retirer au joueur
+     * @throws IllegalArgumentException En cas de montant supérieur au solde du joueur
+     */
     public void reduceCash(int value) {
         if (cash < value) {
-            throw new IllegalArgumentException("Impossible d'avoir du cash en negatif");
+            throw new IllegalArgumentException("Impossible d'avoir du cash en négatif");
         }
         cash -= value;
     }
 
+    /**
+     * Simple getter pour la pièce du joueur
+     *
+     * @return La pièce déplacée par le joueur
+     */
     public Piece getPiece() {
         return piece;
     }
 
+    /**
+     * Simple getter pour l'argent du joueur
+     *
+     * @return Le solde du joueur
+     */
     public int getNetWorth() {
         return cash;
     }
